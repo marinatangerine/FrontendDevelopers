9b5754e95f4289a31cf3b96be459a57e6830b163
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-01-19 20:00:26 +0100

    inclusion de Mock y clases

---
+++ src/app/clases/song.ts
@@ -0,0 +1,11 @@
+export class Song {
+    id: number;
+    title: string;
+    album: string;
+    author: string;
+    style: string;
+    year: string;
+    duration: string;
+    description: string;
+    picture: string;
+  }
---
+++ src/app/mock/mock-songs.ts
@@ -0,0 +1,15 @@
+import { Song } from '../clases/song';
+
+export const SONGS: Song [] = [
+    { 
+        id: 1,
+        title: 'I want to break free',
+        album: 'The Works',
+        author: 'Queen',
+        style: 'Rock',
+        year: '1984',
+        duration: '05:00',
+        description: 'Cuando I Want To Break Free fue lanzado como single en 1984, fue otro éxito para Queen, número 3 en las listas británicas. La canción llegó al Top 10 en la mayoría de las listas de éxitos del mundo, incluso llegó al primer lugar en países como Austria, Bélgica y Holanda',
+        picture: '../../assets/img/queen.jpg',
+    }
+]
