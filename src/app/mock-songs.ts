deecbbc67e79d6f95143fec3e8c3b97f5ca14f08
JordiRafolsMunoz <jrafolsmu@fp.uoc.edu>
2020-01-20 01:02:32 +0100

    [feature/details] Se agregan portadas de discos

--- src/app/mock-songs.ts
+++ src/app/mock-songs.ts
@@ -11,6 +11,7 @@ export const SONGS: Song [] = [
         duration: '05:00',
         description: 'Cuando I Want To Break Free fue lanzado como single en 1984, fue otro éxito para Queen, número 3 en las listas británicas. La canción llegó al Top 10 en la mayoría de las listas de éxitos del mundo, incluso llegó al primer lugar en países como Austria, Bélgica y Holanda',
         picture: '../../assets/img/queen.jpg',
+        cover: '../../assets/img/queen_theworks.jpg',
     },
     
     {
@@ -23,6 +24,7 @@ export const SONGS: Song [] = [
         duration: '03:44',
         description: 'Every Breath You Take es una power ballad grabada e interpretada de la banda británica de rock The Police, perteneciente al álbum Synchronicity (1983). Escrita por Sting, se convirtió en el mayor éxito de 1983, encabezando la lista Billboard de los 100 sencillos más vendidos durante ocho semanas, siendo el único sencillo de la banda en esta tabla; perdurando así hasta hoy como insignia tanto de Sting, como de la banda.',
         picture: '../../assets/img/thepolice.jpg',
+        cover: '../../assets/img/thepolice_synchronicity.jpg',
     },
 
     {
@@ -35,6 +37,7 @@ export const SONGS: Song [] = [
         duration: '08:25',
         description: 'Money for Nothing es una canción interpretada por la banda británica de rock Dire Straits, extraída de su quinto álbum de estudio Brothers in Arms y publicada por la compañía discográfica Vertigo Records el 24 de junio de 1985 como el segundo sencillo del disco. Compuesta por Mark Knopfler en colaboración con Sting,2​ las letras del tema fueron consideradas polémicas desde su lanzamiento, ya que se basan en un hombre de clase trabajadora que se encuentra haciendo videos y comenta sobre ello. La canción cuenta con un cameo de Sting cantando en falsete la introducción y en la parte del coro.',
         picture: '../../assets/img/dire_straits.jpg',
+        cover: '../../assets/img/dire_straits_brothersinarms.jpg',
     },
 
     {
@@ -47,6 +50,7 @@ export const SONGS: Song [] = [
         duration: '04:56',
         description: 'With or Without You es una power ballad de la banda irlandesa de rock U2. Es la tercera canción de su quinto álbum de estudio The Joshua Tree (1987), fue lanzada como el primer sencillo del álbum el 5 de enero de 1987, convirtiéndose de esta manera en el primer sencillo de U2 en alcanzar el n.º 1 en Estados Unidos, donde permaneció durante un plazo de tres semanas.',
         picture: '../../assets/img/u2.jpg',
+        cover: '../../assets/img/u2_thejoshuatree.jpg',
     },
 
     {
@@ -59,6 +63,7 @@ export const SONGS: Song [] = [
         duration: '06:23',
         description: '«Paranoid Android» (en español: «Androide paranoide») es una canción de la banda inglesa de rock alternativo Radiohead, que figura como la segunda pista de su álbum OK Computer de 1997. La letra de la sombría pero intencionadamente humorística canción fue escrita principalmente por el vocalista Thom Yorke, después de una mala experiencia en un bar de Los Ángeles. Con sus seis minutos y veintitrés segundos de duración, la canción está notablemente influenciada por el tema «Happiness Is a Warm Gun» de The Beatles .',
         picture: '../../assets/img/radiohead.jpg',
+        cover: '../../assets/img/radiohead_okcomputer.jpg',
     }
 
 
