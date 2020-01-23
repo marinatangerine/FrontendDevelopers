cbf6413248d7c561dfa1682a283ef89f5599c2f9
marinatangerine <suarezlopezmarina@gmail.com>
2020-01-23 14:51:55 +0100

    Added music playback and controls

--- src/app/components/reproducer/reproducer.component.ts
+++ src/app/components/reproducer/reproducer.component.ts
@@ -9,10 +9,41 @@ import { Song } from '../../song';
 export class ReproducerComponent implements OnInit {
   
   @Input() song: Song;
+  audio: HTMLAudioElement;
+  interval: NodeJS.Timer;
   
   constructor() { }
 
   ngOnInit() {
+    this.audio = new Audio();
   }
 
+  ngOnChanges() {
+    this.audio.src = this.song.audio_url;
+  }
+
+  onPlay() {
+    this.audio.play();
+    this.interval = setInterval(function () { this.audio.play() }, 1000);
+  }
+
+  onPause() {
+    clearInterval(this.interval);
+    this.audio.pause();
+  }
+
+  onStop() {
+    clearInterval(this.interval);
+    this.audio.src = this.song.audio_url;
+  }
+
+  toHumanTime(length) {
+    var minutes = Math.floor(length / 60),
+      seconds_int = length - minutes * 60,
+      seconds_str = seconds_int.toString(),
+      seconds = seconds_int < 10 ? seconds_str.substr(0, 1) : seconds_str.substr(0, 2),
+      time = (minutes < 10 ? "0" + minutes : minutes) + ':' + (seconds_int < 10 ? "0" + seconds : seconds)
+
+    return time;
+  }
 }
